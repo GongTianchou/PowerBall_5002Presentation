3fef5ed04b399b1d7c8255c0710750352b4d6223
Tianchou Gong <77791249+GongTianchou@users.noreply.github.com>
2022-12-02 16:33:05 -0800

    add clear function

--- powerball.js
+++ powerball.js
@@ -1,4 +1,8 @@
 var generateBtn = document.querySelector("#generate");
+var clearBtn = document.querySelector("#clear")
+var count = 0;
+var btn = document.getElementById("generate");
+var disp = document.getElementById("display");
 
 function writepowerball(){
     var password = getRandomInt();
@@ -6,28 +10,39 @@ function writepowerball(){
     passwordText.value = password;
 }
 
-    function getRandomInt(min, whiteMax, redMax) {
-        password = "White Ball Numbers: ";
-        var whiteBallLength = 5
-        var min = 1
-        var whiteMax = 69
-        var redMax = 26
-        for (var i = 0; i < whiteBallLength; i++) {
-            wMin = Math.ceil(min);
-            wMax = Math.floor(whiteMax);
-            password += Math.floor(Math.random() * (wMax - wMin + 1) + wMin);
-            if (i != whiteBallLength - 1) {
-                password += ", ";
-            } else {
-                password += "\n";
-            }
+function getRandomInt(min, whiteMax, redMax) {
+    password = "White Ball Numbers: ";
+    var whiteBallLength = 5
+    var min = 1
+    var whiteMax = 69
+    var redMax = 26
+    for (var i = 0; i < whiteBallLength; i++) {
+        wMin = Math.ceil(min);
+        wMax = Math.floor(whiteMax);
+        password += Math.floor(Math.random() * (wMax - wMin + 1) + wMin);
+        if (i != whiteBallLength - 1) {
+            password += ", ";
+        } else {
+            password += "\n";
         }
-        rMin = Math.ceil(min);
-        rMax = Math.floor(redMax);
-        password += "Power Ball Number: ";
-        password += Math.floor(Math.random() * (rMax - rMin + 1) + rMin);
+    }
+    rMin = Math.ceil(min);
+    rMax = Math.floor(redMax);
+    password += "Power Ball Number: ";
+    password += Math.floor(Math.random() * (rMax - rMin + 1) + rMin);
         
-        return password
-        }
+    return password
+}
+
+      
+btn.onclick = function () {
+    count++;
+    disp.innerHTML = count;
+}
+    
+function cleartimes(){
+    disp.innerHTML = "0";
+}
 
 generateBtn.addEventListener("click", writepowerball);
+clearBtn.addEventListener("click",cleartimes)
